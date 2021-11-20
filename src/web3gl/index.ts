b074769ed3fa4d658d5b5f53b7e0212fcaf98964
leon-do <leondo123@gmail.com>
2021-11-20 13:18:45 -0500

    use ethers and create sendGTS()

--- src/web3gl/index.ts
+++ src/web3gl/index.ts
@@ -1,11 +1,13 @@
 import Onboard from "bnc-onboard";
-import Web3 from "web3";
+import { ethers } from "ethers";
+import { hexlify, parseUnits } from "ethers/lib/utils";
 
-let web3: Web3;
+let provider: ethers.providers.Web3Provider;
 
 // declare types
 declare global {
   interface Window {
+    ethereum: any;
     web3NetworkId: number; // network.js
     infuraKey: string; // network.js
     web3gl: Web3GL;
@@ -30,6 +32,16 @@ interface Web3GL {
     gasLimit: string,
     gasPrice: string
   ) => void;
+  sendGTS: (
+    method: string,
+    abi: string,
+    contract: string,
+    args: string,
+    value: string,
+    gasLimit: string,
+    gasPrice: string
+  ) => void;
+  sendGTSResponse: string;
   sendTransactionResponse: string;
   signMessage: (message: string) => void;
   signMessageResponse: string;
@@ -46,6 +58,8 @@ window.web3gl = {
   sendTransactionResponse: "",
   signMessage,
   signMessageResponse: "",
+  sendGTS,
+  sendGTSResponse: "",
   networkId: window.web3NetworkId,
 };
 
@@ -63,7 +77,7 @@ const onboard = Onboard({
       }
     },
     wallet: (wallet) => {
-      web3 = new Web3(wallet.provider);
+      provider = new ethers.providers.Web3Provider(wallet.provider);
     },
     network: (id) => {
       window.web3gl.networkId = id;
@@ -88,9 +102,7 @@ async function connect() {
   const walletChecked = await onboard.walletCheck();
   if (!walletChecked) window.location.reload();
   initialLogin = false;
-  if ((await web3.eth.net.getId()) === window.web3NetworkId) {
-    window.web3gl.connectAccount = (await web3.eth.getAccounts())[0];
-  }
+  window.web3gl.connectAccount = await provider.getSigner().getAddress();
 }
 
 /*
@@ -99,8 +111,7 @@ window.web3gl.signMessage("hello")
 */
 async function signMessage(message: string) {
   try {
-    const from: string = (await web3.eth.getAccounts())[0];
-    const signature: string = await web3.eth.personal.sign(message, from, "");
+    const signature: string = await provider.getSigner().signMessage(message);
     window.web3gl.signMessageResponse = signature;
   } catch (error: any) {
     window.web3gl.signMessageResponse = error.message;
@@ -113,7 +124,55 @@ const abi = `[ { "inputs": [], "name": "increment", "outputs": [], "stateMutabil
 const contract = "0xB6B8bB1e16A6F73f7078108538979336B9B7341C"
 const args = "[]"
 const value = "0"
-const gasLimit = "222222" // gas limit
+const gasLimit = ""
+const gasPrice = ""
+window.web3gl.sendGTS(method, abi, contract, args, value, gasLimit, gasPrice)
+*/
+async function sendGTS(
+  method: string,
+  abi: string,
+  contract: string,
+  args: string,
+  value: string,
+  gasLimit: string,
+  gasPrice: string
+) {
+  // create contract data
+  const Contract = new ethers.Contract(
+    "0x0000000000000000000000000000000000000000",
+    JSON.parse(abi),
+    provider
+  );
+  const { data } = await Contract.populateTransaction[method](
+    ...JSON.parse(args)
+  );
+  // sign transaction with contract data
+  provider
+    .getSigner()
+    .sendTransaction({
+      from: await provider.getSigner().getAddress(),
+      to: contract,
+      value: parseUnits(value, "wei"),
+      gasLimit: gasLimit ? hexlify(Number(gasLimit)) : undefined,
+      gasPrice: gasPrice ? hexlify(Number(gasPrice)) : undefined,
+      data,
+    })
+    .then((signedContract: any) => {
+      console.log(signedContract);
+      window.web3gl.sendGTSResponse = signedContract;
+    })
+    .catch((error: any) => {
+      window.web3gl.sendGTSResponse = error.message;
+    });
+}
+
+/*
+const method = "increment"
+const abi = `[ { "inputs": [], "name": "increment", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "name": "x", "outputs": [ { "internalType": "uint256", "name": "", "type": "uint256" } ], "stateMutability": "view", "type": "function" } ]`;
+const contract = "0xB6B8bB1e16A6F73f7078108538979336B9B7341C"
+const args = "[]"
+const value = "0"
+const gasLimit = "222222"
 const gasPrice = "333333333333"
 window.web3gl.sendContract(method, abi, contract, args, value, gasLimit, gasPrice)
 */
@@ -126,28 +185,38 @@ async function sendContract(
   gasLimit: string,
   gasPrice: string
 ) {
-  const from = (await web3.eth.getAccounts())[0];
-  new web3.eth.Contract(JSON.parse(abi), contract).methods[method](
+  // create contract data
+  const Contract = new ethers.Contract(
+    "0x0000000000000000000000000000000000000000",
+    JSON.parse(abi),
+    provider
+  );
+  const { data } = await Contract.populateTransaction[method](
     ...JSON.parse(args)
-  )
-    .send({
-      from,
-      value,
-      gas: gasLimit ? gasLimit : undefined,
-      gasPrice: gasPrice ? gasPrice : undefined,
+  );
+  // send transaction with contract data
+  provider
+    .getSigner()
+    .sendTransaction({
+      from: await provider.getSigner().getAddress(),
+      to: contract,
+      value: parseUnits(value, "wei"),
+      gasLimit: gasLimit ? hexlify(Number(gasLimit)) : undefined,
+      gasPrice: gasPrice ? hexlify(Number(gasPrice)) : undefined,
+      data,
     })
-    .on("transactionHash", (transactionHash: any) => {
-      window.web3gl.sendContractResponse = transactionHash;
+    .then((transactionHash: any) => {
+      window.web3gl.sendTransactionResponse = transactionHash;
     })
-    .on("error", (error: any) => {
-      window.web3gl.sendContractResponse = error.message;
+    .catch((error: any) => {
+      window.web3gl.sendTransactionResponse = error.message;
     });
 }
 
 /*
 const to = "0xdD4c825203f97984e7867F11eeCc813A036089D1"
 const value = "12300000000000000"
-const gasLimit = "22222" // gas limit
+const gasLimit = "22222"
 const gasPrice = "33333333333"
 window.web3gl.sendTransaction(to, value, gasLimit, gasPrice);
 */
@@ -157,19 +226,19 @@ async function sendTransaction(
   gasLimit: string,
   gasPrice: string
 ) {
-  const from = (await web3.eth.getAccounts())[0];
-  web3.eth
+  provider
+    .getSigner()
     .sendTransaction({
-      from,
+      from: await provider.getSigner().getAddress(),
       to,
-      value,
-      gas: gasLimit ? gasLimit : undefined,
-      gasPrice: gasPrice ? gasPrice : undefined,
+      value: parseUnits(value, "wei"),
+      gasLimit: gasLimit ? hexlify(Number(gasLimit)) : undefined,
+      gasPrice: gasPrice ? hexlify(Number(gasPrice)) : undefined,
     })
-    .on("transactionHash", (transactionHash: any) => {
+    .then((transactionHash: any) => {
       window.web3gl.sendTransactionResponse = transactionHash;
     })
-    .on("error", (error: any) => {
+    .catch((error: any) => {
       window.web3gl.sendTransactionResponse = error.message;
     });
 }
