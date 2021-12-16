755f4157dc8de2768a8403077e63763b6bb3f687
leon-do <leondo123@gmail.com>
2021-12-15 19:01:45 -0500

    add more networks

--- public/network.js
+++ public/network.js
@@ -1,6 +1,5 @@
 /*
 Used to set the network
-
 1 Mainnet
 3 Ropsten
 4 Rinkeby
@@ -14,6 +13,11 @@ Used to set the network
 80001 Matic Testnet
 43113 Avalanche Testnet
 43114 Avalanche Mainnet
+42220 Celo Mainnet
+44787 Celo Alfajores Testnet
+62320 Celo Baklava Testnet
+1666700000 Harmony Testnet Shard0
+1666600000 Harmony Mainnet Shard0
 */
 
 window.web3NetworkId = 1;
